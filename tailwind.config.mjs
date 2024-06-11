2a0528366854457d07cf3a1fb0f86e5c467872bb
Yae <hitomiyae56@gmail.com>
2024-06-11 11:07:17 -0500

    Add: Colores a Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        azul: "#16304E",
+        azul2: "#1D4069",
+        azul3: "#00558F",
+        azul4: "#A0CFEB",
+        turquesa: "#185166",
+        amarillo: "#DD9C2B",
+        amarillo2: "#F6C11B",
+        gris: "#6C6C6C",
+      },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
       },
